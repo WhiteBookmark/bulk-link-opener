c97e1f42034c46c61a96414fd8fdd024041025ca
Suleman Khan <suleman.mehmood@yahoo.com>
2019-12-14 17:57:56 +0500

    Added "Powered by 8base" logo

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,9 +1,17 @@
 <template>
 	<v-footer app color="background" class="primary--text">
 		<p class="pa-0 my-auto">&copy; 2019 - Present</p>
+		<a href="https://www.8base.com/" target="_blank" class="ml-3" style="text-decoration: none;">
+			<eight-base-logo></eight-base-logo>
+		</a>
 		<v-spacer></v-spacer>
 		<v-btn text tile color="primary" :to="{name: 'about'}" exact @click="Navigation()">About</v-btn>
-		<v-btn text href="https://github.com/WhiteBookmark/bulk-link-opener" target="_blank" @click="NewTab()">
+		<v-btn
+			text
+			href="https://github.com/WhiteBookmark/bulk-link-opener"
+			target="_blank"
+			@click="NewTab()"
+		>
 			<v-icon>{{Icons.mdiGithubBox}}</v-icon>Github
 		</v-btn>
 	</v-footer>
@@ -12,9 +20,14 @@
 <script lang="ts">
 import { Component, Mixins } from "vue-property-decorator";
 import { mdiGithubBox } from "@mdi/js";
-import { Sound } from '@/mixins/sound';
+import { Sound } from "@/mixins/sound";
+import EightBaseLogo from "@/components/EightBaseLogo.vue";
 
-@Component
+@Component({
+  components: {
+    EightBaseLogo
+  }
+})
 export default class FooterBar extends Mixins<Sound>(Sound) {
   get Icons() {
     return {
